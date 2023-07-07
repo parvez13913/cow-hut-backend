5c4c230a6d988e9788607b8adb787d56ff210750
parvez13913 <parvezz13913@gmail.com>
2023-07-07 15:36:32 +0600

    [DONE] Order validaton

--- src/app/modules/order/order.route.ts
+++ src/app/modules/order/order.route.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { OrderController } from './order.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { OrderValidation } from './order.validation';
 
 const router = express.Router();
 
-router.post('/create-order', OrderController.createOrder);
+router.post(
+  '/create-order',
+  validateRequest(OrderValidation.createOrderZodSchema),
+  OrderController.createOrder
+);
 router.get('/', OrderController.getAllOrders);
 
 export const OrderRouter = router;
