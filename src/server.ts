3645d0bd472f47b499b294d95405deac73a991ed
parvez13913 <parvezz13913@gmail.com>
2023-07-09 13:11:15 +0600

    [DONE] assignment

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
+/* eslint-disable no-console */
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
-import { errorLogger, logger } from './app/modules/shared/logger';
 import { Server } from 'http';
 
 process.on('uncaughtException', error => {
-  errorLogger.error(error);
+  console.log(error);
   process.exit(1);
 });
 let server: Server;
@@ -13,18 +13,18 @@ let server: Server;
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
-    logger.info('🛢🛢 Database is Connected Successfully');
+    console.log('🛢🛢 Database is Connected Successfully');
     server = app.listen(config.port, () => {
-      logger.info(`Database listening on port ${config.port}`);
+      console.log(`Database listening on port ${config.port}`);
     });
   } catch (error) {
-    errorLogger.error('Fail to connect Database', error);
+    console.log('Fail to connect Database', error);
   }
 
   process.on('unhandledRejection', error => {
     if (server) {
       server.close(() => {
-        errorLogger.error(error);
+        console.log(error);
         process.exit(1);
       });
     }
@@ -33,7 +33,7 @@ async function bootstrap() {
 bootstrap();
 
 process.on('SIGTERM', () => {
-  logger.info('SIGTERM is received');
+  console.log('SIGTERM is received');
   if (server) {
     server.close();
   }
